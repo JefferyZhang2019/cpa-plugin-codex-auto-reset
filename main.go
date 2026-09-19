865d9c3ceecccd54a37a4f0eb62601971830db86
JefferyZhang <jefferyzhang2019@gmail.com>
2026-09-19 14:15:47 +0800

    fix: default pluginVersion to 0.1.1 (fallback when ldflags absent)

--- main.go
+++ main.go
@@ -70,7 +70,7 @@ var (
 	globalState   = &PluginState{Accounts: map[string]AccountRuntime{}}
 	globalWorker  *Worker
 	globalHandlers *managementHandlers
-	pluginVersion = "0.1.0"
+	pluginVersion = "0.1.1"
 )
 
 func main() {}
